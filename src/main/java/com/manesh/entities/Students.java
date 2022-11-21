28bacef2cb7594e786ce2525fdefa60cb4c40811
manesh13 <maneshborkar13@gmail.com>
2022-11-21 16:54:17 +0530

    Unit test added

--- src/main/java/com/manesh/entities/Students.java
+++ src/main/java/com/manesh/entities/Students.java
@@ -1,41 +1,21 @@
 package com.manesh.entities;
 
-
+import lombok.*;
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@Builder
+@Data
+@AllArgsConstructor
 @Table(name = "Students")
 public class Students {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+
     private String name;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getNumber() {
-        return number;
-    }
-
-    public void setNumber(Long number) {
-        this.number = number;
-    }
-
     private Long number;
 
 }
