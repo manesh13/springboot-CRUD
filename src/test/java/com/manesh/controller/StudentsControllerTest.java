bc186389531615d080e2655ff6d32c99ae042552
manesh13 <maneshborkar13@gmail.com>
2022-12-08 14:55:43 +0530

    minor fixes

--- src/test/java/com/manesh/controller/StudentsControllerTest.java
+++ src/test/java/com/manesh/controller/StudentsControllerTest.java
@@ -1,6 +1,5 @@
 package com.manesh.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manesh.entities.Students;
 import com.manesh.services.StudentsService;
@@ -24,8 +23,6 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
